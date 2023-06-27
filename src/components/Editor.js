cf7c3d12eada61ff97c6cbdb47a7d8516425f15f
Prachi24599 <prachi24599@gmail.com>
2023-06-28 01:06:16 +0530

    expand collapsed functionality

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FaCompressAlt, FaExpandAlt } from "react-icons/fa";
 
 const Editor = (props) => {
   const { language, displayName, value, onChange } = props;
+
+  const [open, setOpen] = useState(true);
 
   function handleChange(editor, data, value) {
     onChange(value);
   }
 
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className="editor-title">
         {displayName}
-        <button>O/C</button>
+        <button
+          type="button"
+          className="expand-collapse-btn"
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          {open ? <FaCompressAlt /> : <FaExpandAlt />}
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
